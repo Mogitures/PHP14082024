90be2962aeca5223aba9046a9ae5f20cbd9972a0
Mogitures <davibeserker@outlook.com.br>
2024-08-14 22:14:31 -0300

    14082024

---
+++ somavalor.php
@@ -0,0 +1,28 @@
+<?php 
+
+	$num1=20.50;
+	$num2=10.15;
+	$resp = 0.0;
+	$a = 0.0;
+	$b = 0.0;
+
+	$b++;
+	$b++;
+	$b++;
+
+	$resp = $num2;
+
+	if ($resp >= $num1) {
+		echo "Resposta é maior que o num1.";
+	}else{
+		echo "Rapaizzzzz!";
+	}
+	while ($a < 10) {
+		$a++;
+	}
+	echo "<p>$a</p>";
+	echo "<p>$b</p>";
+
+
+
+ ?>
